e31e39ed3f3d359b02dde1e90be5e2c1eb1a64e3
ricardosallin <ricardo.sallin@gmail.com>
2025-06-20 23:49:35 -0300

    Update run.bat

--- map-circles/run.bat
+++ map-circles/run.bat
@@ -1,12 +1,13 @@
 @echo off
-rem Mover para a pasta do projeto
-cd /d "CAMINHO\DO\PROJETO"
 
-rem Ativar o ambiente virtual
-call "CAMINHO\DO\PROJETO\venv\Scripts\activate.bat"
+:: Navegar até a pasta do projeto (mude para a sua pasta)
+cd /d "C:\Users\projects\map_circles"
 
-rem Abrir o navegador
-start http://127.0.0.1:5000
+:: Ativar o ambiente virtual
+call venv\Scripts\activate.bat
 
-rem Executar o servidor
+:: Abrir o navegador com a URL padrão
+start http://127.0.0.1:5000 
+
+:: Executar o servidor
 python app.py
